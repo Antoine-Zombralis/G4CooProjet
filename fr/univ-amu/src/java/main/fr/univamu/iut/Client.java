f22b2f20903af332f096d59739b411ede1594dea
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-12 09:32:40 +0100

    client et trader

--- fr/univ-amu/src/java/main/fr/univamu/iut/Client.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Client.java
@@ -1,19 +1,4 @@
 package fr.univamu.iut;
 
-import java.util.List;
-
-public interface Client {
-
-    List<ProduitCommercialisable> mesProduitsAchetés = null;
-
-    public boolean abonne = false;
-
-    public int id = 0;
-
-    CompteBancaire monComte = null;
-
-    public void acheterProduit(ProduitCommercialisable produit);
-
-    public void notifierClient(Client client);
-
+public class Client {
 }
