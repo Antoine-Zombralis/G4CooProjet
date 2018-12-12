7538321b4626e534b7e912d5bcc8284b869cd5bd
SOLA Alexis <s17018568@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-12 09:05:08 +0100

    implémentation de la classe fermier

--- fr/univ-amu/src/java/main/fr/univamu/iut/Arboriculteur.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Arboriculteur.java
@@ -5,15 +5,10 @@ package fr.univamu.iut;
  */
 public class Arboriculteur extends Fermier {
 
-    /**
-     * Default constructor
-     */
     public Arboriculteur(String prenom, String nom, int id, CompteBancaire monCompte) {
         super(prenom, nom, id, monCompte);
     }
-/**
-     * 
-     */
+
     public void void cultiverArbre() {
         // TODO implement here
     }
