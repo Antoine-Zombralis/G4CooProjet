b8f3bd9e4f44c39f10ba5a7892d0bccbfdcc474b
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-12 09:43:55 +0100

    grossiste

--- fr/univ-amu/src/java/main/fr/univamu/iut/Trader.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Trader.java
@@ -2,72 +2,14 @@ package fr.univamu.iut;
 
 import java.util.List;
 
-/**
- * 
- */
-public class Trader implements Client {
+public class Trader extends Client {
 
-    private boolean abonne = false;
-    private int id = 0;
-    private CompteBancaire monComte = null;
     private Boolean travaillerSurMarché;
-    private List<ProduitCommercialisable> mesProduitsAchetés = null;
     private List<ProduitCommercialisable> mesProdtuisEnVente = null;
 
-    public Trader(boolean abonne, int id, CompteBancaire monComte, Boolean travaillerSurMarché, List<ProduitCommercialisable> mesProduitsAchetés, List<ProduitCommercialisable> mesProdtuisEnVente) {
-        this.abonne = abonne;
-        this.id = id;
-        this.monComte = monComte;
+    public Trader(String nom, boolean abonne, int id, CompteBancaire monComte, List<ProduitCommercialisable> mesProduitsAchetés, Boolean travaillerSurMarché, List<ProduitCommercialisable> mesProdtuisEnVente) {
+        super(nom, abonne, id, monComte, mesProduitsAchetés);
         this.travaillerSurMarché = travaillerSurMarché;
-        this.mesProduitsAchetés = mesProduitsAchetés;
-        this.mesProdtuisEnVente = mesProdtuisEnVente;
-    }
-
-    public boolean isAbonne() {
-        return abonne;
-    }
-
-    public void setAbonne(boolean abonne) {
-        this.abonne = abonne;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public CompteBancaire getMonComte() {
-        return monComte;
-    }
-
-    public void setMonComte(CompteBancaire monComte) {
-        this.monComte = monComte;
-    }
-
-    public Boolean getTravaillerSurMarché() {
-        return travaillerSurMarché;
-    }
-
-    public void setTravaillerSurMarché(Boolean travaillerSurMarché) {
-        this.travaillerSurMarché = travaillerSurMarché;
-    }
-
-    public List<ProduitCommercialisable> getMesProduitsAchetés() {
-        return mesProduitsAchetés;
-    }
-
-    public void setMesProduitsAchetés(List<ProduitCommercialisable> mesProduitsAchetés) {
-        this.mesProduitsAchetés = mesProduitsAchetés;
-    }
-
-    public List<ProduitCommercialisable> getMesProdtuisEnVente() {
-        return mesProdtuisEnVente;
-    }
-
-    public void setMesProdtuisEnVente(List<ProduitCommercialisable> mesProdtuisEnVente) {
         this.mesProdtuisEnVente = mesProdtuisEnVente;
     }
 
@@ -78,7 +20,7 @@ public class Trader implements Client {
 
     @Override
     public void notifierClient(Client client){
-        System.out.println("De nouveaux produit ont été mis en vente pour vous " + Client.getNom);
+        System.out.println("De nouveaux produit ont été mis en vente pour vous " + client.getNom());
     }
 
     public void mettreEnVente(ProduitCommercialisable produit) {
