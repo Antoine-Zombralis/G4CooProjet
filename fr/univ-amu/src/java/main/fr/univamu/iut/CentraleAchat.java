03ae2cc316b1c6ee08c29e89202210f39e115d0c
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-12 09:52:07 +0100

    centrale achat

--- fr/univ-amu/src/java/main/fr/univamu/iut/CentraleAchat.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/CentraleAchat.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class CentraleAchat extends Client {
     private List<Client> affiliés ;
+    private Grossiste mesGrossistes ;
 
     public CentraleAchat(String nom, boolean abonne, int id, CompteBancaire monComte, List<ProduitCommercialisable> mesProduitsAchetés, List<Client> affiliés) {
         super(nom, abonne, id, monComte, mesProduitsAchetés);
