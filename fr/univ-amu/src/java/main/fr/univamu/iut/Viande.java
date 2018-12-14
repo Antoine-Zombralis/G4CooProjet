143b4eb9b4b56e28ded9e925074cb226b9641e49
SIX Fabian <s17002787@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-14 17:16:10 +0100

    classe Volaille

--- fr/univ-amu/src/java/main/fr/univamu/iut/Viande.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Viande.java
@@ -1,23 +1,18 @@
 package fr.univamu.iut;
 
-fermier;
 
 import java.util.*;
 
-/**
- * 
- */
 public class Viande extends PropositionProduitFermier {
 
-    /**
-     * Default constructor
-     */
-    public Viande() {
+    public Viande(String propriétaire, String id, boolean bio, boolean conforme, int quantité, int prix, Date datePéremption, int poids) {
+        super(propriétaire, id, bio, conforme, quantité, prix, datePéremption);
+        this.poids = poids;
     }
 
-    /**
-     * 
-     */
     private int poids;
 
+    public int getPoids() {
+        return poids;
+    }
 }
