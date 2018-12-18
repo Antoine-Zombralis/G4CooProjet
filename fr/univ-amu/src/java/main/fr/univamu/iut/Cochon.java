d9e8ca153c908ebd80343941a5b10e7ef07ebc6a
SIX Fabian <s17002787@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-18 09:03:42 +0100

    classe Cochon

--- fr/univ-amu/src/java/main/fr/univamu/iut/Cochon.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Cochon.java
@@ -4,12 +4,9 @@ import java.util.Date;
 
 public class Cochon extends Viande {
 
-    public Cochon(String propriétaire, String id, boolean bio, boolean conforme, int quantité, int prix, Date datePéremption, int poids, int quantité1) {
+    public Cochon(String propriétaire, String id, boolean bio, boolean conforme, int quantité, int prix, Date datePéremption, int poids) {
         super(propriétaire, id, bio, conforme, quantité, prix, datePéremption, poids);
-        this.quantité = quantité1;
     }
-
-    private int quantité;
 
     private enum catégorie{
         LANDRACE_FRANCAIS, BASQUE, GASCON, PIETRAIN
