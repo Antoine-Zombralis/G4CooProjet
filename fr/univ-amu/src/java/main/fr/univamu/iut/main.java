b10b0fe48e56c7557ff6419104da7fd8e12ed763
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-18 10:09:42 +0100

    correction et date

--- fr/univ-amu/src/java/main/fr/univamu/iut/main.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/main.java
@@ -1,7 +1,9 @@
 package fr.univamu.iut;
 
 import java.util.Date;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 public class main {
 
     public static void main(String[] args) {
@@ -11,6 +13,11 @@ public class main {
         Arboriculteur gile = new Arboriculteur("gilou", false, 1, 1, new CompteBancaire(1, 1000));
         Arbre pin = new Arbre(gile.getNom(), "1", true,true, 1,10, date, 20, "Pin");
         gile.produireArbre(pin);
+
+                PropositionProduitFermier produit = new PropositionProduitFermier("Jean", "8", true, true, 8, 5) ;
+                System.out.println(produit.getDatePéremption()) ;
+                System.out.println(produit.isBio());
+            }
 
         for(Arbre objet: gile.getMesProductions()){
             System.out.println(objet.getType());
