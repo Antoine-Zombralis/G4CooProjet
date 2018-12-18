fd22d26fe2fa0452e75259649d1f1ab0dba25627
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-18 10:20:38 +0100

    jpp de github

--- fr/univ-amu/src/java/main/fr/univamu/iut/Viande.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Viande.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Viande extends PropositionProduitFermier {
 
     public Viande(String propriétaire, String id, boolean bio, boolean conforme, int quantité, int prix, Date datePéremption, int poids) {
-        super(propriétaire, id, bio, conforme, quantité, prix, datePéremption);
+        super(propriétaire, id, bio, conforme, quantité, prix);
         this.poids = poids;
     }
 
