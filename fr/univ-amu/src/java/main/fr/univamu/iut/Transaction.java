4c1f6f2950a82b92663de1d6cca9ffadc2aca33d
Alexis-Sola <alexis.sola@etu.univ-amu.fr>
2019-01-06 23:10:09 +0100

    créaton de la transaction

--- fr/univ-amu/src/java/main/fr/univamu/iut/Transaction.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Transaction.java
@@ -1,39 +1,22 @@
 package fr.univamu.iut;
 
+import fr.univamu.iut.Produit.Produits;
 
-import java.util.*;
-
-/**
- * 
- */
 public class Transaction {
 
-    /**
-     * Default constructor
-     */
-    public Transaction() {
+    private int numero;
+    private Fermier vendeur;
+    private Client acheteur;
+    private Produits produits;
+
+    public Transaction(int numero, Fermier vendeur, Client acheteur, Produits produits) {
+        this.numero = numero;
+        this.vendeur = vendeur;
+        this.acheteur = acheteur;
+        this.produits = produits;
     }
 
-    /**
-     * 
-     */
-    public int ref;
-
-    /**
-     * 
-     */
-    public int idClient;
-
-    /**
-     * 
-     */
-    public int idVendeur;
-
-    /**
-     * 
-     */
-    public int montant;
-
-
-
+    public void afficherTransaction(){
+        System.out.println("Une transaction avec " + vendeur.getNom() + " et" + acheteur.getNom() + " sur le produit " + produits.getNom() + " du montant " + produits.getPrix());
+    }
 }
