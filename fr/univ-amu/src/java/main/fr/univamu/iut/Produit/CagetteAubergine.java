4c3b1226903af6fdd35ebc5c5b034b6048ffebb6
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-08 10:46:40 +0100

    configuration des dates de péremptions

--- fr/univ-amu/src/java/main/fr/univamu/iut/Produit/CagetteAubergine.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Produit/CagetteAubergine.java
@@ -1,8 +1,15 @@
 package fr.univamu.iut.Produit;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class CagetteAubergine extends Legume{
 
     public CagetteAubergine(BuilderProduits builder) {
         super(builder);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 4) ;
+        Date date = calendar.getTime();
+        setDatePeremption(date);
     }
 }
