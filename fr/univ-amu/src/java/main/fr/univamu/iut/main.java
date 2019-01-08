57907e6ce21e869130ab10764b17be354010b807
Alexis-Sola <alexis.sola@etu.univ-amu.fr>
2019-01-08 19:22:28 +0100

    conflit2

--- fr/univ-amu/src/java/main/fr/univamu/iut/main.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/main.java
@@ -4,9 +4,11 @@ import fr.univamu.iut.Client.Client;
 import fr.univamu.iut.Client.Grossiste;
 import fr.univamu.iut.Client.Fermier.*;
 import fr.univamu.iut.Produit.*;
-import fr.univamu.iut.Produit.Enum.EnumLabel;
+import fr.univamu.iut.Produit.Arbre.Banzai;
+import fr.univamu.iut.Produit.Arbre.Pommier;
+import fr.univamu.iut.Produit.Arbre.Rosier;
+import fr.univamu.iut.Produit.Arbre.Sapin;
 import fr.univamu.iut.Produit.FruitEtLegume.*;
-import fr.univamu.iut.Produit.Viande.Vache;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,36 +34,43 @@ public class main {
         RépertoireVente repertoireVente = new RépertoireVente() ;
         Client c1 = new Grossiste("c1", false, new CompteBancaire(2, 1000));
 
-
-        for(Vegetal vegetal :  creationProduitHorticulteur()){
-            ((Horticulteur) f1).cultiverVegetal(vegetal);
-            controleur.validerProduit(vegetal);
-            if (vegetal.isConforme()){
-                f1.ajouterCommercialisable(vegetal);
-                if (vegetal instanceof Legume)
-                    repertoireVente.ajouterLegume(vegetal);
-                else if (vegetal instanceof Fruit)
-                    repertoireVente.ajouterFruit(vegetal);
-            }
+//
+//        for(Vegetal vegetal :  creationProduitHorticulteur()){
+//            ((Horticulteur) f1).cultiverVegetal(vegetal);
+//            controleur.validerProduit(vegetal);
+//            if (vegetal.isConforme()){
+//                f1.ajouterCommercialisable(vegetal);
+//                if (vegetal instanceof Legume)
+//                    repertoireVente.ajouterLegume(vegetal);
+//                else if (vegetal instanceof Fruit)
+//                    repertoireVente.ajouterFruit(vegetal);
+//            }
+//        }
+//
+//        f1.afficherCommercialisable();
+//
+//        for(Produits vegetal : f1.getMesProduitsCommercialisable()){
+//            if(vegetal.getId() == 5){
+//                c1.acheterProduit(vegetal, f1);
+//            }
+//        }
+//
+//        System.out.println(c1.getMonComte().getSolde());
+//
+//        Vegetal carotte = new CagetteCarotte.BuilderProduits(10, 4, "Cagette de carotte")
+//                .addConforme(false)
+//                .addQuantiteVegetalDansCagette(25)
+//                .addPrixVegetalUnite(0.75)
+//                .carotteBuild();
+//
+//        System.out.println(carotte.getDatePeremption());
+//
+        List<Produits> tests = new ArrayList<Produits>() ;
+        for(Produits produit : GenerationProduitAleatoire(50, f2)){
+            System.out.println(produit.getNom() + " : " + produit.getQuantite());
         }
 
-        f1.afficherCommercialisable();
 
-        for(Produits vegetal : f1.getMesProduitsCommercialisable()){
-            if(vegetal.getId() == 5){
-                c1.acheterProduit(vegetal, f1);
-            }
-        }
-
-        System.out.println(c1.getMonComte().getSolde());
-
-        Vegetal carotte = new CagetteCarotte.BuilderProduits(10, 4, "Cagette de carotte")
-                .addConforme(false)
-                .addQuantiteVegetalDansCagette(25)
-                .addPrixVegetalUnite(0.75)
-                .carotteBuild();
-
-        System.out.println(carotte.getDatePeremption());
 
     }
 
@@ -122,15 +131,48 @@ public class main {
 
 
     }
-    public List<Produits> GenerationProduitAléatoire(int nbDeProduitsAGénérer, Fermier fermier) {
+    public static List<Produits> GenerationProduitAleatoire(int nbDeProduitsAGenerer, Fermier fermier) {
         List<Produits> produits = new ArrayList<>() ;
         if (fermier instanceof Arboriculteur) {
-            for (int i = 0; i < nbDeProduitsAGénérer; i++) {
+            int cptRosier = 0 ;
+            int cptBanzai = 0 ;
+            int cptSapin = 0 ;
+            int cptPommier = 0 ;
+            for (int i = 0; i < nbDeProduitsAGenerer; i++) {
                 double alea = Math.random() ;
+                if (alea < 0.25) {
+                    ++cptRosier ;
+                }
+                else if (alea > 0.25 && alea <0.5) {
+                    ++cptBanzai ;
+                }
+                else if (alea > 0.50 && alea <0.75) {
+                    ++cptSapin ;
+                }
+                else
+                    ++cptPommier ;
+
+
 
 
             }
+            Rosier rosier = new Rosier.BuilderProduits(cptRosier, 45, "Rosier fleurie")
+                    .rosierBuid();
+            Pommier pommier = new Pommier.BuilderProduits(cptPommier, 45, "Pommier d'Alsace")
+                    .pommierBuild();
+            Sapin sapin = new Sapin.BuilderProduits(cptSapin, 45, "Sapin de Noel")
+                    .sapinBuild();
+            Banzai banzai = new Banzai.BuilderProduits(cptBanzai, 45, "Banzai")
+                    .banzaiBuild();
+            produits.add(rosier) ;
+            produits.add(pommier) ;
+            produits.add(sapin) ;
+            produits.add(banzai) ;
         }
+
+
+
+
         return produits ;
     }
 
