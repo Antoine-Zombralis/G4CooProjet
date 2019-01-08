be8bf6e387e4c9a6e41e77470f867fd3afd45972
TOURNEUR Elouan <t17001350@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-08 11:21:48 +0100

    classes produits laitiers

---
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Produit/Beurre.java
@@ -0,0 +1,14 @@
+package fr.univamu.iut.Produit;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Beurre extends ProduitLaitier {
+    public Beurre(BuilderProduits builder) {
+        super(builder);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 25) ;
+        Date date = calendar.getTime();
+        setDatePeremption(date);
+    }
+}
