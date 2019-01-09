0a6d2494abe949288094bdb8561b6be475627660
Elouan-TOURNEUR <elouan.tourneur@etu.univ-amu.fr>
2019-01-09 20:26:51 +0100

    commentaire compteBancaire

--- fr/univ-amu/src/java/main/fr/univamu/iut/CompteBancaire.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/CompteBancaire.java
@@ -1,31 +1,39 @@
 package fr.univamu.iut;
 
-import fr.univamu.iut.Client.Client;
-import fr.univamu.iut.Client.Fermier.Fermier;
-
-import java.util.*;
-
-
+/**
+ * Classe qui gère les comptes bancaire des clients
+ */
 public class CompteBancaire {
 
     private int idClient;
     private double solde;
 
+    /**
+     * @param idClient
+     * @param solde
+     * Constructeur du compte bancaire qui prend en paramètre un idClient et un solde de base
+     */
     public CompteBancaire(int idClient, int solde) {
         this.idClient = idClient;
         this.solde = solde;
     }
 
-    public double getSolde() {
-        return solde;
-    }
-
+    /**
+     * @param idClient
+     * @param solde (montant à créditer)
+     * Crédite le compte du client (idClient), d'un montant spécifié dans les paramètres (par exemple lors d'une vente)
+     */
     public void crediter(int idClient,double solde) {
         if(this.idClient == idClient){
             this.solde = this.solde + solde;
         }
     }
 
+    /**
+     * @param idClient
+     * @param solde (montant à débiter)
+     * Débite le compte du client (idClient), d'un montant spécifié dans les paramètres (par exemple lors d'un achat)
+     */
     public void debiter(int idClient, double solde) {
         if(this.idClient == idClient){
             this.solde = this.solde - solde;
@@ -39,4 +47,9 @@ public class CompteBancaire {
     public void setSolde(int solde) {
         this.solde = solde;
     }
+
+    public double getSolde() {
+        return solde;
+    }
+
 }
