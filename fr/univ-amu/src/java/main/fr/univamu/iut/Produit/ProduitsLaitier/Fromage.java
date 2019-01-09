e5a16bbc4afc7a39510a92366e80c1d0601a2e81
Fabian six <fabian.six@etu.univ-amu.fr>
2019-01-09 19:12:47 +0100

    Modification de la javadoc

--- fr/univ-amu/src/java/main/fr/univamu/iut/Produit/ProduitsLaitier/Fromage.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Produit/ProduitsLaitier/Fromage.java
@@ -6,6 +6,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Fromage extends ProduitLaitier {
+    /**
+     * @param builder : Correspond au design patern builder
+     * Date correspond à la date de péremption du produit
+     */
     public Fromage(Produits.BuilderProduits builder) {
         super(builder);
         Calendar calendar = Calendar.getInstance();
