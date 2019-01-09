404dc7e1b4c90d7a7fbad43878406503a811c43c
ZOMBRALIS Antoine <z17007611@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 09:57:41 +0100

    suppression des commentaires // des tests

--- fr/univ-amu/src/java/main/fr/univamu/iut/Test/CompteBancaireTest.java
+++ fr/univ-amu/src/java/main/fr/univamu/iut/Test/CompteBancaireTest.java
@@ -1,30 +1,30 @@
-//package fr.univamu.iut.Test;
-//
-//import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
-//
-//
-//import fr.univamu.iut.CompteBancaire;
-//import org.junit.jupiter.api.Test;
-//
-//class CompteBancaireTest {
-//
-//    @Test
-//    final void  crediter() {
-//        CompteBancaire compte = new CompteBancaire(1111, 0);
-//        int res = 5;
-//        compte.crediter(1111, 5);
-//        if (compte.getSolde() != res) {
-//            fail("Le compte n'a pas été crédité");
-//        }
-//    }
-//
-//    @Test
-//    final void debiter() {
-//        CompteBancaire compte = new CompteBancaire(1111, 5);
-//        int res = 0;
-//        compte.debiter(1111, 5);
-//        if (compte.getSolde() != res) {
-//            fail("Le compte n'a pas été débité");
-//        }
-//    }
-//}
+package fr.univamu.iut.Test;
+
+import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
+
+
+import fr.univamu.iut.CompteBancaire;
+import org.junit.jupiter.api.Test;
+
+class CompteBancaireTest {
+
+    @Test
+    final void  crediter() {
+        CompteBancaire compte = new CompteBancaire(1111, 0);
+        int res = 5;
+        compte.crediter(1111, 5);
+        if (compte.getSolde() != res) {
+            fail("Le compte n'a pas été crédité");
+        }
+    }
+
+    @Test
+    final void debiter() {
+        CompteBancaire compte = new CompteBancaire(1111, 5);
+        int res = 0;
+        compte.debiter(1111, 5);
+        if (compte.getSolde() != res) {
+            fail("Le compte n'a pas été débité");
+        }
+    }
+}
